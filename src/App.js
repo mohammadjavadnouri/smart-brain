1921b00ad434e9181d7fbbe9843f903122ec8d1f
Your Name <you@example.com>
2020-11-24 23:15:45 +0330

    front page version 1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
+import React, {Component} from 'react';
+import Particles from 'react-particles-js';
 import './App.css';
+import Navigation from "./components/navigation/Navigation"
+import Logo from "./components/logo/logo";
+import ImageLinkForm from "./components/imageLinkForm/ImageLinkForm"
 
-function App() {
+class App extends Component {
+  render(){
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <Particles 
+          className="particles" 
+          params={{
+            		particles: {
+            			line_linked: {
+            				shadow: {
+            					enable: true,
+            					color: "#3CA9D1",
+            					blur: 15
+            				}
+            			}
+            		}
+            	}}
+          /> 
+      <Navigation />
+       <Logo />
+      <ImageLinkForm />
+      { /*
+      <FaceRecognition /> */}
+    </div> 
   );
 }
+} 
 
 export default App;
