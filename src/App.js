0e4cc6f8120f97ccaedac5ec9a0a6fcfe2c05d1a
Your Name <you@example.com>
2020-11-26 00:47:59 +0330

    Add bounding-box

--- src/App.js
+++ src/App.js
@@ -19,8 +19,27 @@ class App extends Component {
     super();
     this.state = {
       input: '',
-      imageURL: ''
+      imageURL: '',
+      box: {}
     }
+  }
+
+  calculateFaceLocation = (data) =>{
+    const clarifaiFace = data.outputs[0].data.regions[0].region_info.bounding_box;
+    const image = document.getElementById('inputImage');
+    const width = Number(image.width);
+    const height = Number(image.height);
+    return{
+      leftCol : clarifaiFace.left_col * width,
+      topRow : clarifaiFace.top_row * height,
+      rightCol : width - (clarifaiFace.right_col * width),
+      bottomRow : height - (clarifaiFace.bottom_row * height)
+    }
+  }
+
+  displayFaceBox = (box) =>{
+    console.log(box);
+    this.setState({box : box});
   }
 
   onInputChange = (event) => {
@@ -32,16 +51,8 @@ class App extends Component {
     app.models.predict(
       Clarifai.FACE_DETECT_MODEL,
       this.state.input)
-      .then(
-      function(response){
-        //do sth
-        console.log(response.outputs[0].data.regions[0].region_info.bounding_box)
-      },
-      function(err){
-        // if there was an error
-        console.log(err)
-      }
-    );
+      .then(response => this.displayFaceBox(this.calculateFaceLocation(response)))
+      .catch(err => console.log(err))
   }
 
   render(){
@@ -72,6 +83,7 @@ class App extends Component {
       />
       <FaceRecognition 
         imageURL={this.state.imageURL}
+        box={this.state.box}
       />
     </div> 
   );
--- src/components/faceRecognition/FaceRecognition.js
+++ src/components/faceRecognition/FaceRecognition.js
@@ -1,16 +1,12 @@
 import React from 'react';
+import './FaceRecognition.css';
 
-
-const FaceRecognition = ({imageURL}) =>{
+const FaceRecognition = ({imageURL, box}) =>{
     return(
         <div style={{display: "flex", justifyContent: 'center'}}>
             <div className="absolute mt2">
-                <img 
-                alt="" 
-                src={imageURL}
-                width='600px'
-                height='auto'
-                />
+                <img alt="" src={imageURL} width='600px' height='auto' id='inputImage'/>
+                <div className="bounding-box" style={{top: box.topRow, bottom: box.bottomRow, left:box.leftCol, right:box.rightCol}}></div>
             </div>
         </div>
     )
